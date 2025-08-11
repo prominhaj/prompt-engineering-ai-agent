8e4f154dc6a272b14ec935f2ded418582f03806a
prominhaj <parsonal322532@gmail.com>
2025-08-11 23:52:03 +0600

    Enhance ThemeSwitcher component with mounted state handling and prevent SSR issues. Add useCarousel export to carousel component.

--- components/theme-swicher.tsx
+++ components/theme-swicher.tsx
@@ -1,10 +1,16 @@
 "use client"
 
+import { useEffect, useState } from "react";
 import { ThemeSwitcher as ThemeSwitcherComponent } from '@/components/ui/kibo-ui/theme-switcher';
-import { useTheme } from "next-themes"
+import { useTheme } from "next-themes";
 
 export default function ThemeSwitcher() {
     const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => setMounted(true), []);
+
+    if (!mounted) return null;
 
     return (
         <div className='fixed top-4 right-4'>
@@ -14,5 +20,5 @@ export default function ThemeSwitcher() {
                 value={(theme as 'light' | 'dark' | 'system') ?? 'system'}
             />
         </div>
-    )
+    );
 }
